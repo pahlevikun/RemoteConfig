99c5cadbbbf4f6ae38da0267d869808ee5b8f2e6
Farhan Yuda Pahlevi <pahlevi.kun@gmail.com>
2018-02-15 14:46:35 +0700

    Enable developer mode

--- app/src/main/java/com/deggan/remoteconfig/MainActivity.kt
+++ app/src/main/java/com/deggan/remoteconfig/MainActivity.kt
@@ -2,6 +2,7 @@ package com.deggan.remoteconfig
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.widget.Toast
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig
 import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings
@@ -15,7 +16,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val remoteConfig = FirebaseRemoteConfig.getInstance()
-        val remoteConfigSettings = FirebaseRemoteConfigSettings.Builder().build()
+        val remoteConfigSettings = FirebaseRemoteConfigSettings.Builder()
+                .setDeveloperModeEnabled(true)
+                .build()
 
         remoteConfig.setConfigSettings(remoteConfigSettings)
         remoteConfig.fetch(0)
@@ -27,6 +30,7 @@ class MainActivity : AppCompatActivity() {
                         textViewMessage.text = remoteConfig.getString("deggan_app_message")
                         textViewVersion.text = remoteConfig.getLong("deggan_app_version").toString()
                     } else {
+                        Log.d("HASIL", task.exception!!.toString())
                         Toast.makeText(this@MainActivity, "Fetch Failed", Toast.LENGTH_SHORT).show()
                     }
                 })
