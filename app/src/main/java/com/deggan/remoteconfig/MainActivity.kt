6cafd19e905f947f300a684072cd58893ea6b982
Farhan Yuda Pahlevi <pahlevi.kun@gmail.com>
2018-02-15 13:34:09 +0700

    SetUp remote config

--- app/src/main/java/com/deggan/remoteconfig/MainActivity.kt
+++ app/src/main/java/com/deggan/remoteconfig/MainActivity.kt
@@ -2,11 +2,24 @@ package com.deggan.remoteconfig
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val remoteConfig = FirebaseRemoteConfig.getInstance()
+        val remoteConfigSettings = FirebaseRemoteConfigSettings.Builder().build()
+
+        remoteConfig.setConfigSettings(remoteConfigSettings)
+
+        var configMap = HashMap<String,String>()
+        configMap["deggan_end_point"] = "https://deggan.com"
+        configMap["deggan_app_version"] = "1"
+        configMap["deggan_app_message"] = "Hai Dunia!"
     }
 }
